5f5f21dc49d2c83b055ae8b1eeb4b8dbede5ea65
Kev <kevin@MacBookPro.lan>
2024-10-29 23:16:57 +0100

    exo_tasks : toDoList ok

--- exo_tasks/assets/tasks.js
+++ exo_tasks/assets/tasks.js
@@ -1,6 +1,45 @@
-const create = document.getElementById('create');
 const inputTask = document.getElementById('inputTask');
 const submit = document.getElementById('submit');
+const error = document.getElementById('errorMessage');
 const toDoList = document.getElementById('toDoList');
 const doneList = document.getElementById('doneList');
+
+// Penser à rajouter un message d'erreur sous l'input si on essaye de 
+// créer une tâche qui existe déjà
+
+submit.addEventListener('click', function() {
+    if (toDoList.textContent.toLocaleLowerCase().includes(inputTask.value.toLocaleLowerCase())) {
+        error.textContent = 'Cette tâche est déjà présente en base';
+    } else {
+
+        const toDoTasks = document.createElement('li');
+
+        //Affichage de la tâche 
+        const taskText = document.createElement('span');
+        taskText.textContent = inputTask.value;
+        toDoTasks.appendChild(taskText);
+
+        //Bouton terminé 
+        const finishedBtn = document.createElement('button');
+        finishedBtn.textContent = 'Terminé';
+        finishedBtn.classList.add('finished')
+
+        //Bouton supprimer
+        const suppressBtn = document.createElement('button');
+        suppressBtn.textContent = 'Supprimer';
+        suppressBtn.classList.add('delete');
+
+        
+
+        toDoTasks.appendChild(finishedBtn);
+        toDoTasks.appendChild(suppressBtn);
+
+        //Ajout de l'élément 'li' complet avec le span 
+        toDoList.appendChild(toDoTasks)
+        //vide champ de saisie après ajout
+        inputTask.value = '';
+    }
+})
+
+
 
