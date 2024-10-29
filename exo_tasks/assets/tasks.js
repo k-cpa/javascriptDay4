6b828c8bf696ee703d855b1b4b0fd6d715fc9d8e
Kev <kevin@MacBook-Pro-de-Kev.local>
2024-10-29 16:39:32 +0100

    exo_tasks added but i haven't really start working on it

---
+++ exo_tasks/assets/tasks.js
@@ -0,0 +1,6 @@
+const create = document.getElementById('create');
+const inputTask = document.getElementById('inputTask');
+const submit = document.getElementById('submit');
+const toDoList = document.getElementById('toDoList');
+const doneList = document.getElementById('doneList');
+
